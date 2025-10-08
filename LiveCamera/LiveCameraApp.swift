71b1ef33c7e24f45846c56da457c76032831505f
Angel Docampo <mail@angeldocampo.com>
2025-10-08 12:46:44 +0200

    Initial commit

---
+++ LiveCamera/LiveCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCameraApp.swift
+//  LiveCamera
+//
+//  Created by Angel Docampo on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
